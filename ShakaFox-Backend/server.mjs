8530f6e27167fa821e4da8917771f36fd218fd87
Payden <91985814+PayK47@users.noreply.github.com>
2025-04-29 01:32:10 +0000

    last one

--- ShakaFox-Backend/server.mjs
+++ ShakaFox-Backend/server.mjs
@@ -3,7 +3,7 @@ import express from 'express';
 import fetch from 'node-fetch';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Enable CORS
 import cors from "cors";
